8c1bc62efb876c565315caa01dc6d61c4b77e179
dmorn <danielmorandini@me.com>
2022-10-21 18:44:43 +0200

    Initialize repo

---
+++ lib/membrane/live_framerate_converter.ex
@@ -0,0 +1,18 @@
+defmodule Membrane.LiveFramerateConverter do
+  @moduledoc """
+  Documentation for `Membrane.LiveFramerateConverter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Membrane.LiveFramerateConverter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/membrane/live_framerate_converter_test.exs
@@ -0,0 +1,8 @@
+defmodule Membrane.LiveFramerateConverterTest do
+  use ExUnit.Case
+  doctest Membrane.LiveFramerateConverter
+
+  test "greets the world" do
+    assert Membrane.LiveFramerateConverter.hello() == :world
+  end
+end
